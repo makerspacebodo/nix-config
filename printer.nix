fa7fb48c8bfa106335ec3124d44a2d4ef06fb29b
Common Maker <makerspacebodo@gmail.com>
2024-12-21 23:51:11 +0100

    add octoprint plugins, harden, zen kernel

--- printer.nix
+++ printer.nix
@@ -2,6 +2,10 @@
 
 # Configuration for systems connected to 3D-printers
 {
+
+  # high-performance Linux kernel to ensure timely operation of printer communicatio
+  boot.kernelPackages = pkgs.linuxPackages_zen; 
+
   # Packages
   environment.systemPackages = with pkgs; [
     git
@@ -14,8 +18,33 @@
     openFirewall = true;
     plugins = plugins: with plugins; [
       navbartemp
+      bedlevelvisualizer
+      displaylayerprogress
+      mqtt
+      printtimegenius
+      resource-monitor
+      simpleemergencystop
+      dashboard
     ];
   };
+
   ## Remote shell for administration of systems without display
   services.openssh.enable = true;
+
+  # Auto system update
+  system.autoUpgrade = {
+    enable = true;
+  };
+
+  # Automatic Garbage Collection
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 60d";
+  };
+
+  ## Harden security
+  # Disable CUPS printing server
+  services.printing.enable = false;
+
 }
