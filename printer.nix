0f48c0205f1abcc581869faf1f9a9af64fe49356
Common Maker <makerspacebodo@gmail.com>
2025-03-11 20:27:50 +0100

    add caddy, home-assistant; rtkit

--- printer.nix
+++ printer.nix
@@ -27,6 +27,49 @@
       dashboard
     ];
   };
+  
+  # networking config
+  networking.firewall.allowedTCPPorts = [ 443 80 8123 ];
+
+  # Home Assistant container
+  virtualisation.oci-containers = {
+    backend = "podman";
+    containers.homeassistant = {
+      volumes = [
+        "home-assistant:/config"
+        "/etc/nixos/nix-config/home-assistant:/config/extra"
+      ];
+      environment.TZ = "Europe/Oslo";
+      image = "ghcr.io/home-assistant/home-assistant:stable";
+      extraOptions = [ 
+        "--network=host" 
+        "--device=/dev/serial/by-id/usb-Texas_Instruments_TI_CC2531_USB_CDC___0X00124B0018DF32C1-if00:/dev/serial/by-id/usb-Texas_Instruments_TI_CC2531_USB_CDC___0X00124B0018DF32C1-if00"  # zigbee radio
+      ];
+    };
+  };
+
+
+  # Caddy reverse proxy
+  services.caddy = {
+    enable = true;
+    virtualHosts."octoprint.lan".extraConfig = ''
+      reverse_proxy http://localhost:5000
+      tls internal
+    '';
+    virtualHosts."home-assistant.lan".extraConfig = ''
+      reverse_proxy http://localhost:8123
+      tls internal
+    '';
+  };
+
+
+  # Performance tuning
+  ## Automated tuning
+  services.bpftune.enable = true;
+
+  ## let processes get real-time scheduling on demand
+  security.rtkit.enable = true;
+
 
   ## Remote shell for administration of systems without display
   services.openssh.enable = true;
@@ -42,6 +85,7 @@
     dates = "weekly";
     options = "--delete-older-than 60d";
   };
+
 
   ## Harden security
   # Disable CUPS printing server
